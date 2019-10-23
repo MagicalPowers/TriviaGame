6c700dbe00754703b53d93bdd93d287750aadb3d
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2019-10-23 10:53:37 -0400

    typed out some basic instructions

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// i will create a trivia form with multiple choice or true/false options (your choice).
+//     the player will have a limited amount of time to finish the quiz. 
+//     the game ends when the time tuns out. the page will reveal the number of questions that players answer correctly and incorrectly.
+//     dont let the player pick more than one answer per question.
+//     dont forget to include a countdown timer. 
+
+//     make a multi-question, multiple choice trivia-stlye game. 
+//     With a timer that will "close" the trivia test. 
+//     And a (DONE) button that will do exactly the same function as the timer.
+//     radio button
+//     dynamically code with jQuery
+
+//     link to repo || github page link || portfolio w/ link link>>>>>
