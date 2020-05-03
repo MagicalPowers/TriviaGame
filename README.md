f62b236f89caa2975c1234b60b1bc6ba4204e66d
SuperPowers <53920220+MagicalPowers@users.noreply.github.com>
2020-05-03 06:28:15 -0400

    correcting errors, removing debris, changing layout

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TriviaGame
 homework due friday oct25 2019
+
+https://magicalpowers.github.io/TriviaGame/
+
+essential concept here is use of javascript, jQuery, specifically timerouts/timers, 
+
+for the radio button concept I got a lot of help from this gentleman: https://www.youtube.com/watch?v=qio50ujL2nA 
+
+everything here is at best derivative of other people's code, however this is a learning experience for me. this is practice.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,61 @@
-// i will create a trivia form with multiple choice or true/false options (your choice).
-//     the player will have a limited amount of time to finish the quiz. 
-//     the game ends when the time tuns out. the page will reveal the number of questions that players answer correctly and incorrectly.
+//     create a trivia form with multiple choice or true/false options.
+//     the player will have a limited amount of time, the game ends when the time tuns out. the page will reveal the questions answered correctly.
 //     dont let the player pick more than one answer per question.
-//     dont forget to include a countdown timer. 
-
-//     make a multi-question, multiple choice trivia-stlye game. 
-//     With a timer that will "close" the trivia test. 
+//     dont forget to include a countdown timer.
 //     And a (DONE) button that will do exactly the same function as the timer.
-//     radio button
-//     dynamically code with jQuery
-
-//     link to repo || github page link || portfolio w/ link link>>>>>
-
+//     radio buttons, dynamically code with jQuery
 
 //i know i need some objects containing questions
 
-//this is stolen from w3schools.com
-//this is a thing, a function that will alert, after sixty seconds, some sort of message to the user
-//eventually this should run a function that "The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly." but i wouldnt bet on that outcome.
-
-//okay, let's take a step back. 
-//just watched a video, first result on youtube?, anyway this guy basically lays out exactly what one 'should' do to create a radio button quiz. for reference: https://www.youtube.com/watch?v=qio50ujL2nA
-// anyway, all of this follows me watching that^ and thus should reflect that.
-
 var global1 = ["What is the first answer?"];
-var global2 = ["<input onclick=XXX1() name=eins type=radio />2<br /><input onclick=XXXX1() name=eins type=radio />4<br />"];
+var global2 = [
+  "<input onclick=XXX1() name=eins type=radio />2<br /><input onclick=XXXX1() name=eins type=radio />4<br />",
+];
 
 var global3 = ["What is the first answer?"];
-var global4 = ["<input onclick=XXX2() name=zwei type=radio />2<br /><input onclick=XXXX2() name=zwei type=radio />4<br />"];
+var global4 = [
+  "<input onclick=XXX2() name=zwei type=radio />2<br /><input onclick=XXXX2() name=zwei type=radio />4<br />",
+];
 
 var global5 = ["What is the first answer?"];
-var global6 = ["<input onclick=XXX3() name=drei type=radio />2<br /><input onclick=XXXX3() name=drei type=radio />4<br />"];
+var global6 = [
+  "<input onclick=XXX3() name=drei type=radio />2<br /><input onclick=XXXX3() name=drei type=radio />4<br />",
+];
 
 var global7 = ["What is the first answer?"];
-var global8 = ["<input onclick=XXX4() name=vier type=radio />2<br /><input onclick=XXXX4() name=vier type=radio />4<br />"];
+var global8 = [
+  "<input onclick=XXX4() name=vier type=radio />2<br /><input onclick=XXXX4() name=vier type=radio />4<br />",
+];
 
 var global9 = ["What is the first answer?"];
-var global10 = ["<input onclick=XXX5() name=funf type=radio />2<br /><input onclick=XXXX5() name=funf type=radio />4<br />"];
+var global10 = [
+  "<input onclick=XXX5() name=funf type=radio />2<br /><input onclick=XXXX5() name=funf type=radio />4<br />",
+];
 
 var global11 = ["What is the first answer?"];
-var global12 = ["<input onclick=XXX6() name=sechs type=radio />2<br /><input onclick=XXXX6() name=sechs type=radio />4<br />"];
+var global12 = [
+  "<input onclick=XXX6() name=sechs type=radio />2<br /><input onclick=XXXX6() name=sechs type=radio />4<br />",
+];
 
 var global13 = ["What is the first answer?"];
-var global14 = ["<input onclick=XXX7() name=sieben type=radio />2<br /><input onclick=XXXX7() name=sieben type=radio />4<br />"];
+var global14 = [
+  "<input onclick=XXX7() name=sieben type=radio />2<br /><input onclick=XXXX7() name=sieben type=radio />4<br />",
+];
 
 var global15 = ["What is the first answer?"];
-var global16 = ["<input onclick=XXX8() name=acht type=radio />2<br /><input onclick=XXXX8() name=acht type=radio />4<br />"];
+var global16 = [
+  "<input onclick=XXX8() name=acht type=radio />2<br /><input onclick=XXXX8() name=acht type=radio />4<br />",
+];
 
 var global17 = ["What is the first answer?"];
-var global18 = ["<input onclick=XXX9() name=neun type=radio />2<br /><input onclick=XXXX9() name=neun type=radio />4<br />"];
+var global18 = [
+  "<input onclick=XXX9() name=neun type=radio />2<br /><input onclick=XXXX9() name=neun type=radio />4<br />",
+];
 
-var global19 = ["What is the first answer?"];
-var global20 = ["<input onclick=XXX10() name=zehn type=radio />2<br /><input onclick=XXXX10() name=zehn type=radio />4<br />"];
+var global19 = ["What is the second answer?"];
+var global20 = [
+  "<input onclick=XXX10() name=zehn type=radio />2<br /><input onclick=XXXX10() name=zehn type=radio />4<br />",
+];
 
 var global21;
 var global22;
@@ -60,27 +64,37 @@ var global24;
 var global25;
 
 //now, a function, which i think should load the first question-answer set
-//i suppose i coule use a jQuery function here
 window.onload = function () {
   global21 = 0;
-  questiontrash1.innerHTML = global1;
-  optiontrash1.innerHTML = global2;
-
+  //yeah, these should set the HTML of these elements to the first set of questions/answers
+  question1.innerHTML = global1;
+  option1.innerHTML = global2;
+  //and this should load a button to the 'submit' element, and run an on-click function. labeled Proceed
+  submit.innerHTML = "<button onclick=functionA()>Proceed</button>";
+};
+//this will be the submit button function
+function functionA() {
+  //establish a base score of zero, so we dont get any NaN
+  global22 = 0;
+  //load the next set
+  question1.innerHTML = global3;
+  option1.innerHTML = global4;
+  // now for some sort of sumbit button action
+  submit.innerHTML = "<button onclick=functionB()>Proceed</button>";
 }
 //this is a function that should run when a correct answer is chosen
+//using a variable for score storage
 function function1() {
+  //ill set a hard number as the score, rather than use a global21++, because I don't want wanton score manipulation.
   global21 = 1;
-
 }
 //this is a function that should run when an incorrect answer is chosen
 function function2() {
   global21 = 0;
 }
 
-
-
-
 function myFunction() {
-    setTimeout(function(){ alert("somesort of message to the user"); }, 60000);
-  }
-
+  setTimeout(function () {
+    alert("somesort of message to the user");
+  }, 60000);
+}
